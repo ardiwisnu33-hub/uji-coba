c0ab1acfa648e3732c28b1ce216a2a5a338596a4
Wisnu <ardiwisnu33@gmail.com>
2026-01-23 13:23:30 +0700

    layoutinfg

--- resources/views/siswa/edit.blade.php
+++ resources/views/siswa/edit.blade.php
@@ -1,113 +1,92 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Edit Data Siswa</title>
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-</head>
-<body>
-    <header class="header" style="background-color: rgb(92, 51, 240); align-items: center;">
-        <h1 style="color: white; text-align: center;">Edit Data Siswa</h1>
-        <nav class="navbar">
-            <ul style="list-style-type: none; text-decoration: none; display: flex;">
-                <li><a href="{{ url('/index') }}" style="color: white; text-decoration: none; margin: 5px; font-family: sans-serif;">Home</a></li>
-                <li><a href="{{ url('/data-siswa') }}" style="color: white; text-decoration: none; margin: 5px; font-family: sans-serif;">Data Siswa</a></li>
-                <li><a href="{{ url('/about') }}" style="color: white; text-decoration: none; margin: 5px; font-family: sans-serif;">About</a></li>
-            </ul>
-        </nav>
-    </header>
-
-    <div class="container mt-3">
-        <form action="{{ url('/siswa/update/'.$siswa->id) }}" method="post" enctype="multipart/form-data">
-            @csrf
-            @method('put')
-            <div class="mb-3">
-                <label for="nama" class="form-label">Nama</label>
-                <input type="text" class="form-control @error('nama')
-                    is-invalid
-                @enderror" name="nama" id="nama" value="{{ old('nama', $siswa->nama) }}" />
-                @error('nama')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
+@extends('layouts.back')
+@section('title','edit')
+@section('content')
+    
+<form action="{{ url('/siswa/update/'.$siswa->id) }}" method="post" enctype="multipart/form-data">
+    @csrf
+    @method('put')
+    <div class="mb-3">
+        <label for="nama" class="form-label">Nama</label>
+        <input type="text" class="form-control @error('nama')
+        is-invalid
+        @enderror" name="nama" id="nama" value="{{ old('nama', $siswa->nama) }}" />
+        @error('nama')
+        <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
+    </div>
             <div class="mb-3">
                 <label for="kelas" class="form-label">Kelas</label>
                 <input type="text" class="form-control @error('kelas')
-                    is-invalid
+                is-invalid
                 @enderror" name="kelas" id="kelas" value="{{ old('kelas', $siswa->kelas) }}">
                 @error('kelas')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="mb-3">
                 <label for="jurusan" class="form-label">Jurusan</label>
                 <input type="text" class="form-control @error('jurusan')
-                    is-invalid
+                is-invalid
                 @enderror" name="jurusan" id="jurusan" value="{{ old('jurusan', $siswa->jurusan) }}">
                 @error('jurusan')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="mb-3">
                 <label for="jenis_kelamin" class="form-label">Jenis Kelamin</label>
                 <select class="form-control @error('jenis_kelamin')
-                    is-invalid
+                is-invalid
                 @enderror" name="jenis_kelamin" id="jenis_kelamin">
-                    <option value="Laki-laki" {{ $siswa->jenis_kelamin == 'Laki-laki' ? 'selected' : null }}>Laki-laki</option>
-                    <option value="Perempuan" {{ $siswa->jenis_kelamin == 'Perempuan' ? 'selected' : null }}>Perempuan</option>
-                </select>
-                @error('jenis_kelamin')
+                <option value="Laki-laki" {{ $siswa->jenis_kelamin == 'Laki-laki' ? 'selected' : null }}>Laki-laki</option>
+                <option value="Perempuan" {{ $siswa->jenis_kelamin == 'Perempuan' ? 'selected' : null }}>Perempuan</option>
+            </select>
+            @error('jenis_kelamin')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="mb-3">
                 <label for="tempat_lahir" class="form-label">Tempat Lahir</label>
                 <input type="text" class="form-control @error('tempat_lahir')
-                    is-invalid
+                is-invalid
                 @enderror" name="tempat_lahir" id="tempat_lahir" value="{{ old('tempat_lahir', $siswa->tempat_lahir) }}">
                 @error('tempat_lahir')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="mb-3">
                 <label for="tanggal_lahir" class="form-label">Tanggal Lahir</label>
                 <input type="date" class="form-control @error('tanggal_lahir')
-                    is-invalid
+                is-invalid
                 @enderror" name="tanggal_lahir" id="tanggal_lahir" value="{{ old('tanggal_lahir', $siswa->tanggal_lahir->format('Y-m-d')) }}">
                 @error('tanggal_lahir')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="mb-3">
                 <label for="alamat" class="form-label">Alamat</label>
                 <textarea class="form-control @error('alamat')
-                    is-invalid
+                is-invalid
                 @enderror" name="alamat" id="alamat">{{ old('alamat', $siswa->alamat) }}</textarea>
                 @error('alamat')
                     <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
-                        @isset($data->foto)
-                            <img src="{{ asset('foto_siswa' . '/' . $data->foto) }}" alt="foto {{ $data->nama }}"
-                                class="img-thumbnail" style="width: 20%;">
-                        @endisset
-                        <div class="mb-3">
-                            <label for="foto" class="form-label">Foto Siswa</label>
-                            <input type="file" name="foto" id="foto"
-                                class="form-control @error('foto') is-invalid @enderror" value="{{ old('foto') }}">
-                            @error('foto')
-                                <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror
-                        </div>
-            <div class="text-center mt-3">
-                <a href="{{ url('/data-siswa') }}" class="btn btn-secondary">Kembali</a>
-                <button type="submit" class="btn btn-success">Simpan</button>
-            </div>
-        </form>
-    </div>
-
-</body>
-</html>
+                    @enderror
+                </div>
+                @isset($data->foto)
+                <img src="{{ asset('foto_siswa' . '/' . $data->foto) }}" alt="foto {{ $data->nama }}"
+                class="img-thumbnail" style="width: 20%;">
+                @endisset
+                <div class="mb-3">
+                    <label for="foto" class="form-label">Foto Siswa</label>
+                    <input type="file" name="foto" id="foto"
+                    class="form-control @error('foto') is-invalid @enderror" value="{{ old('foto') }}">
+                    @error('foto')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="text-center mt-3">
+                    <a href="{{ url('/data-siswa') }}" class="btn btn-secondary">Kembali</a>
+                    <button type="submit" class="btn btn-success">Simpan</button>
+                </div>
+            </form>
+            
+@endsection
--- resources/views/siswa/tambah.blade.php
+++ resources/views/siswa/tambah.blade.php
@@ -1,100 +1,83 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Data Siswa</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-</head>
-<body>
-    <header class="header" style="background-color: rgb(92, 51, 240); align-items: center;">
-        <h1 style="color: white; text-align: center;">Tambah Data Siswa</h1>
-        <nav class="navbar">
-            <ul style="list-style-type: none; text-decoration: none; display: flex;">
-                <li><a href="{{ url('/index') }}" style="color: white; text-decoration: none; margin: 5px; font-family: sans-serif;">Home</a></li>
-                <li><a href="{{ url('/data-siswa') }}" style="color: white; text-decoration: none; margin: 5px; font-family: sans-serif;">Data Siswa</a></li>
-                <li><a href="{{ url('/about') }}" style="color: white; text-decoration: none; margin: 5px; font-family: sans-serif;">About</a></li>
-            </ul>
-        </nav>
-    </header>
-    <div class="container mt-4">
-    <form action="{{ url('/siswa/tambah') }}" method="post" enctype="multipart/form-data">
-            @csrf
-            <div class="mb-3">
-                <label for="nama" class="form-label">Nama</label>
-                <input type="text" class="form-control @error('nama')
+@extends('layouts.back')
+@section('title', 'tambah')
+@section('content')
+    
+<form action="{{ url('/siswa/tambah') }}" method="post" enctype="multipart/form-data">
+    @csrf
+    <div class="mb-3">
+        <label for="nama" class="form-label">Nama</label>
+        <input type="text" class="form-control @error('nama')
                     is-invalid
                 @enderror" name="nama" id="nama"  />
                 @error('nama')
                     <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="kelas" class="form-label">Kelas</label>
-                <input type="text" class="form-control @error('kelas')
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="kelas" class="form-label">Kelas</label>
+                    <input type="text" class="form-control @error('kelas')
                     is-invalid
                 @enderror" name="kelas" id="kelas">
                 @error('kelas')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="mb-3">
                 <label for="jurusan" class="form-label">Jurusan</label>
                 <input type="text" class="form-control @error('jurusan')
-                    is-invalid
+                is-invalid
                 @enderror" name="jurusan" id="jurusan">
                 @error('jurusan')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="mb-3">
                 <label for="jenis_kelamin" class="form-label">Jenis Kelamin</label>
                 <select class="form-control @error('jenis_kelamin')
-                    is-invalid
+                is-invalid
                 @enderror" name="jenis_kelamin" id="jenis_kelamin">
-                    <option value="">Pilih Jenis Kelamin</option>
-                    <option value="Laki-laki">Laki-laki</option>
-                    <option value="Perempuan">Perempuan</option>
-                </select>
-                @error('jenis_kelamin')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="mb-3">
+                <option value="">Pilih Jenis Kelamin</option>
+                <option value="Laki-laki">Laki-laki</option>
+                <option value="Perempuan">Perempuan</option>
+            </select>
+            @error('jenis_kelamin')
+            <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
                 <label for="tempat_lahir" class="form-label">Tempat Lahir</label>
                 <input type="text" class="form-control @error('tempat_lahir')
-                    is-invalid
+                is-invalid
                 @enderror" name="tempat_lahir" id="tempat_lahir">
                 @error('tempat_lahir')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="mb-3">
                 <label for="tanggal_lahir" class="form-label">Tanggal Lahir</label>
                 <input type="date" class="form-control @error('tanggal_lahir')
                     is-invalid
-                @enderror" name="tanggal_lahir" id="tanggal_lahir">
-                @error('tanggal_lahir')
+                    @enderror" name="tanggal_lahir" id="tanggal_lahir">
+                    @error('tanggal_lahir')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="mb-3">
                 <label for="alamat" class="form-label">Alamat</label>
                 <input type="text" class="form-control @error('alamat')
-                    is-invalid
+                is-invalid
                 @enderror" name="alamat" id="alamat">
                 @error('alamat')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="mb-3">
                 <label for="foto" class="form-label">Foto</label>
                 <input type="file" class="form-control @error('foto')
-                    is-invalid
+                is-invalid
                 @enderror" id="foto" name="foto">
                 @error('foto')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="text-center">
@@ -102,6 +85,4 @@
                 <button type="submit" class="btn btn-success">Simpan</button>
             </div>
         </form>
-        </div>
-</body>
-</html>
+@endsection        
